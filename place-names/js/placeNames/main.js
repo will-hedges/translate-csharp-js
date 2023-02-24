432e4b50aa95fff1596e10196a32480e18957e3f
Will Hedges <wshedges@outlook.com>
2023-02-24 09:52:34 -0600

    completed placeNames.js

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,12 +1,28 @@
 // Put your code here
 
 const names = [
-   "Nashville", "Hong Kong", "The back yard", "Earth",
-    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
+  "Nashville",
+  "Hong Kong",
+  "The back yard",
+  "Earth",
+  "London",
+  "The mall",
+  "Ryman Auditorium",
+  "The Great Wall of China",
 ];
 
-console.log("All Pace Names");
+console.log("All Place Names");
 
 for (const name of names) {
+  console.log(name);
+}
+
+console.log("");
+
+const theNames = names.filter((name) => name.startsWith("The"));
+
+console.log("'The' Place Names");
+
+for (const name of theNames) {
   console.log(name);
 }

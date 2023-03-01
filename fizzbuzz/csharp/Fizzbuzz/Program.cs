3b89d0614f15f7882abc50a6315b4b993814eeb0
Will Hedges <wshedges@outlook.com>
2023-03-01 09:23:09 -0600

    completed Fizzbuzz in csharp

---
+++ fizzbuzz/csharp/Fizzbuzz/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+Main();
+
+void Main()
+{
+    for (int n = 1; n <= 100; n++)
+    {
+        if (n % 15 == 0)
+        {
+            Console.WriteLine("fizzbuzz");
+        }
+        else if (n % 3 == 0)
+        {
+            Console.WriteLine("fizz");
+        }
+        else if (n % 5 == 0)
+        {
+            Console.WriteLine("buzz");
+        }
+        else
+        {
+            Console.WriteLine(n);
+        }
+    }
+}
